19dbba0576a52135c0ff73abee4f071655aca8ce
RobbertTamminga <45688843+RobbertTamminga@users.noreply.github.com>
2020-06-23 15:30:09 +0200

    startup script v1

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-name="mynameisjef"
-
-echo $name
+if [! saltstack];
+    then
+        curl -L https://bootstrap.saltstack.com -o install_salt.sh
+        sudo sh install_salt.sh
+        echo "master: 10.0.0.28" > /etc/salt/minion.conf
+fi
